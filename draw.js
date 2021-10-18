e37d9dda073f68a4386b69a1a9b1e39f24a329ae
scisaif <saiflll284@gmail.com>
2021-10-18 06:59:22 +0530

    increased grid size

--- draw.js
+++ draw.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext("2d");
-const scale = 30;
+const scale = 20;
 const rows = canvas.height / scale;
 const columns = canvas.width / scale;
 
